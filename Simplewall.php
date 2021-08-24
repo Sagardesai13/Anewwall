37f5ad5a03acbb5d8a906e85cb19a281d4d7bc1d
Sagar Desai <74409690+Sagardesai13@users.noreply.github.com>
2021-08-24 19:21:20 +0530

    Initial commit

---
+++ Simplewall.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+<title>A new wall</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark pb-3 pt-3 ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">A New Wall</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class=" collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav  mb-2 mb-lg-0">
+        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        <a class="nav-link" href="index.php#aboutus">About Us</a>
+        <a class="nav-link" href="index.php#ourservice">Our Servicies</a>
+        <a class="nav-link" href="index.php#gallary">Gallary</a>
+        <a class="nav-link" href="index.php#contactus">Contact Us</a>
+
+
+
+      </div>
+    </div>
+  </div>
+</nav>
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Simple Wall <hr class="width:60% margin-left:auto margin-right:auto" /></h2>
+
+  </div>
+  <div class="container-fluid">
+  <div class="row pb-5 ">
+    <div class="col-lg-5 col-md-5 col-12">
+        <div class="aboutimg pb-5"><img src="image/img26.jfif" class="img-fluid " ></div>
+    </div>
+
+      <div class="col-lg-6 col-md-6 col-12">
+          <p class="py-3 "> WallMantra is dedicated to add value to your life through beautiful and affordable wall decor. Art is a form of expression.It is also considered as a type of therapy and the way that affects your mood. Colors in art paintings is known to enhance the mood. Vibrant colors will often give a person a feeling of full of positive energy. <p>
+            <a href="index.php#contactus" class="btn btn-success"> Inquiry </a>
+      </div>
+  </div>
+
+</div>
+
+</section>
+
+<section class="my-5">
+  <div class="py-5">
+  <div class="pt-5">
+  <h2 class="text-center pt-5">More Ideas</h2><hr></div>
+</div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/img11.jpg" class="img-fluid pb-4">
+    </div>  <div class="col-lg-4 col-md-4 col-12">
+  <img src="image/img31.jpg" class="img-fluid pb-4">
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+    <img src="image/img23.jpg" class="img-fluid pb-4">
+        </div>
+          <div class="col-lg-4 col-md-4 col-12">
+      <img src="image/img16.jpg" class="img-fluid pb-4">
+          </div>
+            <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/img17.jpg" class="img-fluid pb-4">
+            </div>
+              <div class="col-lg-4 col-md-4 col-12">
+          <img src="image/img15.jpg" class="img-fluid pb-4">
+              </div>
+
+                <div class="col-lg-4 col-md-4 col-12">
+            <img src="image/img24.jpg" class="img-fluid pb-4">
+                </div>
+
+                  <div class="col-lg-4 col-md-4 col-12">
+              <img src="image/img32.jpg" class="img-fluid pb-4">
+                  </div>
+
+                    <div class="col-lg-4 col-md-4 col-12">
+                <img src="image/img33.jpg" class="img-fluid pb-4">
+                    </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+              <img src="image/img27.jfif" class="img-fluid pb-4">
+                  </div>
+
+                    <div class="col-lg-4 col-md-4 col-12">
+                <img src="image/img34.jfif" class="img-fluid pb-4">
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-12">
+                <img src="image/img13.jpg" class="img-fluid pb-4">
+                    </div>
+  </div>
+</div>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark pb-3 pt-3 ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">A New Wall</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class=" collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav  mb-2 mb-lg-0">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#aboutus">About Us</a>
+        <a class="nav-link" href="#ourservice">Our Servicies</a>
+        <a class="nav-link" href="#gallary">Gallary</a>
+        <a class="nav-link" href="#contactus">Contact Us</a>
+
+
+
+      </div>
+    </div>
+  </div>
+</nav>
+<div id="demo" class="carousel slide pt-1" data-ride="carousel">
+
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+  </ul>
+
+  <div class="carousel-inner">
+
+    <div class="carousel-item active">
+      <img src="image/img12.png" alt="Gray">
+
+    </div>
+<div class="carousel-item">
+     <img src="image/img13.jpg" alt="Red">
+
+   </div>
+
+     <div class="carousel-item">
+       <img src="image/home.jpg" alt="Green">
+
+   </div>
+ </div>
+
+ <a class="carousel-control-prev" href="#demo" data-slide="prev">
+   <span class="carousel-control-prev-icon"></span>
+ </a>
+ <a class="carousel-control-next" href="#demo" data-slide="next">
+   <span class="carousel-control-next-icon"></span>
+ </a>
+</div>
+</div>
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center" id="aboutus">About Us <hr class="width:60% margin-left:auto margin-right:auto" /></h2>
+
+  </div>
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-5 col-md-5 col-12">
+        <div class=" aboutimg pb-5"><img src="image/picture.jfif" class="img-fluid " ></div>
+    </div>
+
+      <div class="col-lg-6 col-md-6 col-12">
+          <h2>A new wall</h2>
+          <p class="py-3"> Shruti jagtap and Sagar Desai. <p>
+            <a href="about/php" class="btn btn-success"> Know more </a>
+      </div>
+  </div>
+
+</div>
+
+</section>
+<section class="my-5">
+  <div class="py-5">
+    <div class="pt-5">
+    <h2 class="text-center pt-5" id="ourservice">Our services</h2><hr></div>
+</div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" >
+        <img class="card-img-top" src="image/img.jpg" alt="Card image">
+        <div class="card-body">
+          <h4 class="card-title">Decorative Wall</h4>
+          <p class="card-text">A wall with paintings and awsome aesthetic designs</p>
+          <a href="decorative.php" class="btn btn-primary">Get Idea</a>
+        </div>
+      </div>
+
+    </div>
+
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+          <img class="card-img-top" src="image/img4.jpg" alt="Card image">
+          <div class="card-body">
+            <h4 class="card-title">Artistic Wall</h4>
+            <p class="card-text">A wall with artistic design.</p>
+            <a href="artisticwall.php" class="btn btn-primary">Get Idea</a>
+          </div>
+        </div>
+
+      </div>
+
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+            <img class="card-img-top" src="image/img19.jpg" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">Simple Wall</h4>
+              <p class="card-text">A wall with beautiful texture</p>
+              <a href="Simplewall.php" class="btn btn-primary">Gei Idea</a>
+            </div>
+          </div>
+
+        </div>
+  </div>
+</div>
+</section>
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center" id="gallary">Gallary <hr></h2>
+</div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+<img src="image/img2.jpg" class="img-fluid pb-4">
+    </div>  <div class="col-lg-4 col-md-4 col-12">
+  <img src="image/img18.jfif" class="img-fluid pb-4">
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+    <img src="image/img3.jpg" class="img-fluid pb-4">
+        </div>
+          <div class="col-lg-4 col-md-4 col-12">
+      <img src="image/img9.jpg" class="img-fluid pb-4">
+          </div>
+            <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/img46.jpg" class="img-fluid pb-4">
+            </div>
+              <div class="col-lg-4 col-md-4 col-12">
+          <img src="image/img8.jpg" class="img-fluid pb-4">
+              </div>
+  </div>
+</div>
+<div class="w-50 m-auto">
+
+  <section class="my-5">
+    <div class="py-5">
+      <h2 class="text-center"  id="contactus">Contact Us</h2>
+  <form action="userinfo.php" method="post">
+      <div class="form-group">
+          <label> Phone number </label>
+          <input type="text" name="mobile" autocomplete="off" class="form-control" >
+          <label>  name </label>
+          <input type="text" name="name" autocomplete="off" class="form-control" >
+          <label> Adress </label>
+          <input type="text" name="adress" autocomplete="off" class="form-control" >
+          <div class="pt-3 center">
+          <button type="submit" class="btn  btn-outline-dark ">call back</button>
+      </div>  </div>
+      </div>
+
+  </form>
+
+</div>
+</section>
+<footer>
+  <p class="p-3 bg-dark text-white text-center"> By @affairofcolors</p>
+</footer>
+<script type="text/javascript">
+  function sendEmail(){
+$.ajax({url:'userinfo.php}',method: 'POST',dataType:'json',
+
+
+success: function(response){}
+)
+</script>
+</body>
+</html>
