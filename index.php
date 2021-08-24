a036e336d575ee0190a603d49940da93cbe2fabe
Sagar Desai <74409690+Sagardesai13@users.noreply.github.com>
2021-08-24 19:50:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,27 +16,35 @@
 
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark pb-3 pt-3 ">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">A New Wall</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class=" collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav  mb-2 mb-lg-0">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#aboutus">About Us</a>
-        <a class="nav-link" href="#ourservice">Our Servicies</a>
-        <a class="nav-link" href="#gallary">Gallary</a>
-        <a class="nav-link" href="#contactus">Contact Us</a>
-
-
-
-      </div>
-    </div>
-  </div>
-</nav>
-<div id="demo" class="carousel slide pt-1" data-ride="carousel">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">A New Wall</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <<a class="nav-link" href="#aboutus">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#ourservice">Our Servicies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#gallary">Gallary</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contactus">Contact Us</a>
+                    </li>
+            </div>
+        </div>
+    </nav>
+    <div id="demo" class="carousel slide pt-1" data-ride="carousel">
 
   <ul class="carousel-indicators">
     <li data-target="#demo" data-slide-to="0" class="active"></li>
