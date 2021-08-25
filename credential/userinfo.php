752f2926fd81ab85681042614f51928e050a08c7
shruti942 <jagtapshruti2610@gmail.com>
2021-08-25 23:12:08 +0530

    Testing new changes

--- credential/userinfo.php
+++ credential/userinfo.php
@@ -10,7 +10,7 @@ use PHPMailer\PHPMailer\PHPMailer;
   require_once "PHPMailer/SMTP.php";
   require_once "PHPMailer/Exception.php";
 
-sendmail("sahilsu1998@gmail.com","Inquiry from customer","Sample body");
+sendmail("shadowhokage666@gmail.com","Inquiry from customer","Sample body");
 echo "Calling for 1st";
 sendmail("jagtapshruti2610@gmail.com","Inquiry from customer","Sample body");
 echo "Calling for 2nd";
