1705383f9477163732d1795f2808a9f75644e7f3
shruti942 <jagtapshruti2610@gmail.com>
2021-08-25 21:31:16 +0530

    contact us

---
+++ aboutus.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+          <link rel="stylesheet" href="style.css">
+<title>A new Wall </title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+<body style="font-family: 'Open Sans', sans-serif;">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark pb-3 pt-3 ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">A New Wall</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class=" collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav  mb-2 mb-lg-0">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link" href="index.php#aboutus">About Us</a>
+          <a class="nav-link" href="index.php#ourservice">Our Servicies</a>
+          <a class="nav-link" href="index.php#gallary">Gallary</a>
+          <a class="nav-link" href="index.php#contactus">Contact Us</a>
+
+
+
+
+      </div>
+    </div>
+  </div>
+</nav>
+
+<div class="bg-light">
+  <div class="container py-5">
+    <div class="row h-100 align-items-center py-5">
+      <div class="col-lg-6">
+        <h1 class="display-4">About us </h1>
+        </div>
+      <div class="col-lg-6 d-none d-lg-block"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834136/illus_kftyh4.png" alt="" class="img-fluid"></div>
+    </div>
+  </div>
+</div>
+
+<div class="bg-white py-5">
+  <div class="container py-5">
+    <div class="row align-items-center mb-5">
+      <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+        <h2 class="font-weight-light">Welcome to A New Wall</h2>
+        <p class=" mb-4" style="text-align:justify">Welcome to A New Wall, your number one source for paintting service.authors of this organization is Shruti Jagtap and Sagar Desai. We are dedicated to providing you the very best of wall painting, with an emphasis on artistic work, wall decoration.
+        A new Wall has come a long way from its beginnings in Ahemedabad. When Shruti Jagtap and Sagar Desai first started out, their passion for artistic work drove them to start their own business.
+        We hope you enjoy our service as much as we enjoy offering them to you.</p>  </div>
+      <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="image/myself2.jpeg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
+    </div>
+    <div class="row align-items-center">
+      <div class="col-lg-5 px-5 mx-auto"><img src="image/myself1.jpeg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
+      <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
+        <h2 class="font-weight-light">Our Story</h2>
+        <p class="text-justify text-muted mb-4" style="text-align:justify">This organization is for craftsman who needs to show their specialty and were giving them work. this thought came from seeing a craftsman searching for work we wanted to help them. These days individuals like their home to look current even little dividers can look present day. We give them preparing and do rehearse all the more so that individuals can have good outcomes. We propose plans to our customer also we give fundamental beautifications to divider. We don't turn out just for cash, we likewise work for to show our ability. If you have any questions or comments, please don't hesitate to contact us</p>
+ <p>
+        </p>
+        <a href="index.php#contactus" class="btn btn-light px-5 rounded-pill shadow-sm">Contact Us</a>
+        <ul class="social mb-0 list-inline mt-3">
+          <li class="list-inline-item"><a href="#" class="social-link"><i class="fab fa-google"></i></a></li>
+          <li class="list-inline-item"><a href="#" class="social-link"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="bg-dark py-3">
+  <div class="container text-center">
+    <p class="text-white">By @affairofcolors.</p>
+  </div>
+</footer>
+</body>
+</html>
