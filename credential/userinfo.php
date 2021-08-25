33913a21d7b47b0a6bccdd5fc3ec53f8db29a709
shruti942 <jagtapshruti2610@gmail.com>
2021-08-25 22:36:40 +0530

    credentials

--- credential/userinfo.php
+++ credential/userinfo.php
@@ -1,45 +1,4 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-
-  $subject = "Query from customer";
-  $mobile = $_POST['mobile'];
-  $name=$_POST['name'];
-  $message= "hjkihh";
-  $headers="From: anew.wall0@gmail.com";
-  require_once "PHPMailer/PHPMailer.php";
-  require_once "PHPMailer/SMTP.php";
-  require_once "PHPMailer/Exception.php";
-  require_once __DIR__.'../.env';
-  $mail=new PHPMailer();
-$mail->isSMTP();
-$mail->Host ="smtp.gmail.com";
-$mail->SMTPAuth = true ;
-$mail->Username = getenv('EMAIL');
-$mail->Password = getenv('PASSWORD');
-$mail->Port=465;
-$mail->SMTPSecure="ssl";
-$mail->isHTML(true);
-$mail->addAddress("desai.sagar2002@gmail.com");
-$mail->Subject = ("$subject");
-$mail->Body= $name. " ".$mobile;
-  if($mail->send())
-    {echo"Sucess";
-$mail->addAddress(null);
-    }
-  else {
-    echo"cannot send mail";
-  }
-$subject="Your response has been submitted";
-$message= "Thankyou for joining with us.We will call you soon";
-$email=$_POST['email'];
-$mail->addAddress("jagtapshruti2610@gmail.com");
-$mail->Subject = ("$subject");
-$mail->Body= $message;
-if($mail->send())
-  {echo"Sucess";}
-
-else {
-  echo"cannot send mail";
-}
-
+$name->Username = getenv('EMAIL');
+echo $name;
  ?>
