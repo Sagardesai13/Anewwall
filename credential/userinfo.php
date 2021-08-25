9591d0123d8e9e0c45de9096ab613705a76b7030
shruti942 <jagtapshruti2610@gmail.com>
2021-08-25 23:26:44 +0530

    Testing new changes

--- credential/userinfo.php
+++ credential/userinfo.php
@@ -15,7 +15,7 @@ $body=$mobile." ".$name;
 $email=$_POST['email'];
 sendmail("shadowhokage666@gmail.com","Reminder for call ".$name,$body);
 echo "Calling for 1st";
-sendmail($email,"You will be notified soon","Thankyou ".$name." for joining with us.We will call you soon");
+sendmail($email,$name.", You will be notified soon","Thankyou for joining with us.We will call you soon");
 echo "Calling for 2nd";
 function sendmail($email,$subject,$body)
 {
